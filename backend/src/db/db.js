8e9192f113d051661452c66cecc0b96d044312fc
Dtorof <92270837+Dtorof@users.noreply.github.com>
2022-11-29 14:13:21 -0500

    Update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv/config'
 import { database, username, password, host, dialect, port} from '../helpers/helper.js'
 
 
-export const db = new Sequelize(database, username, password, {
-    host,
-    dialect,
-    port
+export const db = new Sequelize('railway', 'postgres', 'iyRattc7ceAL6BKVlXUI', {
+    host:'containers-us-west-106.railway.app',
+    dialect:'postgres',
+    port:6123
   })
 
