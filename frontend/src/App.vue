9892d9db0b6cbbde41dfd8858874bd5b742f5ff1
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-11-30 00:23:49 -0500

    desing navbar,sidebar and menu commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Menu from '../src/components/navigation/Menu.vue'
 
 </script>
 
 <template>
   
-
+   <Menu/>
   <RouterView />
 </template>
 
---
+++ frontend/src/components/navigation/Menu.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Navbar from '../navigation/Navbar.vue'
+import Sidebar from '../navigation/Sidebar.vue'
+</script>
+<template>
+    <Navbar/>
+    <Sidebar/>
+</template>
