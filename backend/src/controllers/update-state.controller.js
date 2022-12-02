48209fd3f4877723a0ab629cee4bccde3cbc1048
drbobby27 <envioshseq@gmail.com>
2022-12-02 10:04:00 -0500

    update-state

--- backend/src/controllers/update-state.controller.js
+++ backend/src/controllers/update-state.controller.js
@@ -17,8 +17,8 @@ export const editResponse = async (req,res) => {
 
             const resData = searchByid.seats_distribution
 
-            const result = (...arr) => arr.map(element => {
-                const ticket = data.find(item => item === element.ID)
+            const result = arr => arr.map(element => {
+                const ticket = (...data) =>  data.find(item => item === element.ID)
                 if(ticket === element.ID) {
                     element["State"] = "Unavailable"
                 }
